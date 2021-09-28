229de9fd8f659426a69101a05da15401e54f2aba
Paul Garrett <LHSRobotics7166@gmail.com>
2021-09-28 18:30:02 -0400

    Added a comment

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,6 +14,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
+
+ //ADDING A COMMENT
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
